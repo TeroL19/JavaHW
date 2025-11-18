1cf5c3bb4c70f4c076fb54b80ad93f225b84f667
TeroL19 <135858343+TeroL19@users.noreply.github.com>
2025-11-18 09:56:30 +0300

    Update HW5_1_3.java

--- m5_1/HW5_1_3.java
+++ m5_1/HW5_1_3.java
@@ -1,5 +1,11 @@
 package m5_1;
 
+public class HW5_1_3 {
+    public static void main(String[] args) {
+        new Outer().test();
+    }
+}
+
 class Outer {
     class Inner {
         private int secret = 100;
@@ -8,13 +14,7 @@ class Outer {
 
     void test() {
         Inner in = new Inner();
-        // System.out.println(in.secret); // ошибка — private
-        System.out.println(in.open); // public — доступ разрешён
-    }
-}
-
-public class HW5_1_3 {
-    public static void main(String[] args) {
-        new Outer().test();
+        // System.out.println(in.secret); // ошибка - private
+        System.out.println(in.open); // public - доступ разрешён
     }
 }
